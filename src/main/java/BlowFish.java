a997c955c82a86f585fc165988d5812fb6baa7ab
Vlad <vladyslav.utkin@icompass.pl>
2018-07-02 21:17:50 +0200

    BlowFish

--- src/main/java/BlowFish.java
+++ src/main/java/BlowFish.java
@@ -4,6 +4,7 @@ import java.nio.charset.StandardCharsets;
 import java.security.NoSuchAlgorithmException;
 import javax.crypto.Cipher;
 import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 import sun.misc.BASE64Decoder;
 import sun.misc.BASE64Encoder;
@@ -15,22 +16,26 @@ public class BlowFish {
 
   static {
     try {
-      cipher = Cipher.getInstance(String.format("%s/%s/%s", algorithm, BlockCipher.ECB, Padding.PKCS5PADDING));
+      cipher = Cipher.getInstance(String.format("%s/%s/%s", algorithm, BlockCipher.CTR, Padding.PKCS5PADDING));
     } catch (NoSuchAlgorithmException | NoSuchPaddingException e) {
       e.printStackTrace();
     }
   }
 
-  static String encrypt(String key, String toEncrypt) throws Exception {
+  static String encrypt(String key, byte[] initVector, String toEncrypt) throws Exception {
+    IvParameterSpec iv = new IvParameterSpec(initVector);
     SecretKeySpec skeySpec = new SecretKeySpec(key.getBytes(StandardCharsets.UTF_8), algorithm);
-    cipher.init(Cipher.ENCRYPT_MODE, skeySpec);
-    byte[] hasil = cipher.doFinal(toEncrypt.getBytes());
+//    cipher.init(Cipher.ENCRYPT_MODE, skeySpec);
+    cipher.init(Cipher.DECRYPT_MODE, skeySpec, iv);
+    byte[] hasil = cipher.doFinal(toEncrypt.getBytes("UTF-8"));
     return new BASE64Encoder().encode(hasil);
   }
 
-  static String decrypt(String key, String toDecrypt) throws Exception {
+  static String decrypt(String key, byte[] initVector, String toDecrypt) throws Exception {
+    IvParameterSpec iv = new IvParameterSpec(initVector);
     SecretKeySpec skeySpec = new SecretKeySpec(key.getBytes(StandardCharsets.UTF_8), algorithm);
-    cipher.init(Cipher.DECRYPT_MODE, skeySpec);
+//    cipher.init(Cipher.ENCRYPT_MODE, skeySpec);
+    cipher.init(Cipher.DECRYPT_MODE, skeySpec, iv);
     byte[] hasil = cipher.doFinal(new BASE64Decoder().decodeBuffer(toDecrypt));
     return new String(hasil);
   }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,23 +1,28 @@
 import java.io.IOException;
 import java.nio.charset.Charset;
+import java.security.SecureRandom;
+import javax.crypto.Cipher;
 import utility.File;
 
 public class Main {
 
   public static void main(String[] args) throws Exception {
     String key = "Bar12345Bar12345"; // 128 bit key
-    String initVector = "RandomInitVector"; // 16 bytes IV
+    String initVector16 = "RandomInitVector"; // 16 bytes IV
+    SecureRandom random = new SecureRandom();
+    byte[] initVector8 = random.generateSeed(8);
+//    String initVector8 = iv.toString(); // 8 bytes IV
 
     String textToEncrypt = File.readFile("/home/vlad/IdeaProjects/Cryptology/src/main/resources/file.txt", Charset.defaultCharset());
     double start, end;
     final int quantity = 100;
     int count = quantity;
 
-//    System.out.println(AES.decrypt(key, initVector, AES.encrypt(key, initVector, "AION")));
+//    System.out.println(AES.decrypt(key, initVector16, AES.encrypt(key, initVector16, "AION")));
 //
 //    start = System.currentTimeMillis();
 //    while (count-- > 0) {
-//      AES.encrypt(key, initVector, textToEncrypt);
+//      AES.encrypt(key, initVector16, textToEncrypt);
 //    }
 //    end = System.currentTimeMillis();
 //
@@ -25,11 +30,11 @@ public class Main {
 //
 //
 //    count = quantity;
-//    String encrypted = AES.encrypt(key, initVector, textToEncrypt);
+//    String encrypted = AES.encrypt(key, initVector16, textToEncrypt);
 //
 //    start = System.currentTimeMillis();
 //    while (count-- > 0) {
-//      AES.decrypt(key, initVector, encrypted);
+//      AES.decrypt(key, initVector16, encrypted);
 //    }
 //    end = System.currentTimeMillis();
 //
@@ -37,11 +42,11 @@ public class Main {
 
 
     /*BLOWFISH*/
-    System.out.println(BlowFish.decrypt(key, BlowFish.encrypt(key, "AION")));
+    System.out.println(BlowFish.decrypt(key, initVector8, BlowFish.encrypt(key, initVector8, "AION")));
 
     start = System.currentTimeMillis();
     while (count-- > 0) {
-      BlowFish.encrypt(key, textToEncrypt);
+      BlowFish.encrypt(key, initVector8, textToEncrypt);
     }
     end = System.currentTimeMillis();
 
@@ -49,15 +54,16 @@ public class Main {
 
 
     count = quantity;
-    String encrypted = BlowFish.encrypt(key, textToEncrypt);
+    String encrypted = BlowFish.encrypt(key, initVector8, textToEncrypt);
 
     start = System.currentTimeMillis();
     while (count-- > 0) {
-      BlowFish.decrypt(key, encrypted);
+      BlowFish.decrypt(key, initVector8, encrypted);
     }
     end = System.currentTimeMillis();
 
     System.out.println("Average of decrypt: " + (end - start) / quantity);
+
 
   }
 
